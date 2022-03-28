b4722077cdd9da2e5c87739c670670b0accced66
Balthazar Gronon <git@balthazar.dev>
2022-03-27 21:26:36 -0600

    Fix chain regen

--- bin/generate.js
+++ bin/generate.js
@@ -255,8 +255,8 @@ const main = async () => {
       for (let i = 0; i < assetsLength; ++i) {
         const hash = assets[i]
         if (
-          full._remap[`${chain}.${hash}`] ||
-          (!get(filters, `${chain}.${hash}`) && !get(filters, `${chain}.undefined`))
+          Object.keys(filters[chain]).length !== 1 &&
+          (full._remap[`${chain}.${hash}`] || !get(filters, `${chain}.${hash}`))
         ) {
           continue
         }
